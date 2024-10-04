60ee969027934859c494f2b13134043da73d982d
Sergei Borja <sergei.borja0107@gmail.com>
2024-10-03 19:35:21 -0600

    Finished Project.

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,5 @@ export default defineConfig({
             }
         }
     },
-    // TODO: Change to git repository
-    // base   : "//" 
+    base   : "/single-page-developer-portfolio/" 
 });
